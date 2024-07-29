8ffa4ac219025a062347c686c7660979af6794e9
gustavoaparecido1104 <109361069+gustavoaparecido1104@users.noreply.github.com>
2024-07-29 13:38:29 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,3 +14,21 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 3000); // Muda de imagem a cada 3 segundos
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.querySelectorAll('.scroll-animation');
+
+    function checkVisibility() {
+        elements.forEach(el => {
+            const rect = el.getBoundingClientRect();
+            if (rect.top < window.innerHeight && rect.bottom >= 0) {
+                el.classList.add('visible');
+            } else {
+                el.classList.remove('visible');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', checkVisibility);
+    checkVisibility(); // Verifica a visibilidade no carregamento
+});
